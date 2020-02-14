15ebd2c118efda07c3561920df420e42fd125dae
JitteryMan <freedomdsk@gmail.com>
2020-02-14 12:31:34 +0300

    Updating

--- Blog/views.py
+++ Blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.utils.translation import gettext as _
-from .models import Post
+from .models import Post, Tag
+
+
+def init_with_tag() -> dict:
+    return {'tags': Tag.objects.order_by('tag_name')}
 
 
 def index(request):
-    post_list = Post.objects.order_by('-title')
-    return render(request, 'Blog/page.html', {'post_list': post_list})
+    list_ = init_with_tag()
+    list_.update({'post_list': Post.objects.order_by('-title')})
+    return render(request, 'Blog/page.html', list_)
 
 
 def detail(request, post_uuid):
     try:
-        uuid = Post.objects.get(uuid=post_uuid)
-    except:
+        list_ = init_with_tag()
+        list_.update({'uuid': Post.objects.get(uuid=post_uuid)})
+    except Exception:
         raise Http404('Статья не найдена!')
-    return render(request, 'Blog/page-detail.html', {'uuid':uuid})
+    return render(request, 'Blog/page-detail.html', list_)
 
 
 def django(request):
